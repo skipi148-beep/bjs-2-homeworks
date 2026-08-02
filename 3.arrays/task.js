b9314743535601c48053e0c5a88046eb31ef6e8d
Skipi <skipi148@gmail.com>
2026-08-02 09:10:22 +0300

    Решено домашнее задание к лекции 3: Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  // Проверяем совпадение длины и равенство элементов на одинаковых индексах
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  // 1. Фильтруем пользователей по полу
+  // 2. Преобразуем массив пользователей в массив их возрастов
+  // 3. Считаем среднее арифметическое через reduce (сумму делим на количество)
+  // Если исходный отфильтрованный массив пустой, оператор || вернет 0
+  const result = users
+    .filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((acc, age, index, array) => acc + age / array.length, 0);
+
+  return result;
 }
